a9938d76b6b2746ff79f02dbbfa18fa46643b3cd
tsipilai <tommi.sipilainen@gmail.com>
2019-07-07 20:28:57 +0300

    2.11 ready

--- part_1/puhelinluottelo/src/App.js
+++ part_1/puhelinluottelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({newFilter, handleChangeFilter}) => {
   return (
@@ -35,12 +36,7 @@ const Persons = ({rows}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
@@ -67,6 +63,14 @@ const App = () => {
   const handleChangeFilter = (event) => {
     setNewFilter(event.target.value)
   }
+
+  useEffect( () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const rows = persons.filter((person) => person.name.toUpperCase().includes(newFilter.toUpperCase())).map((person, key) => 
     <p key={key}>{person.name} {person.number}</p>
